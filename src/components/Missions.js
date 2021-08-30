66d85091de52dbb5b0e6c4644a3726155f5431e1
arthurGC <aguzmanc@unsa.edu.pe>
2021-08-30 18:40:26 -0500

    Add missions container component

---
+++ src/components/Missions.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Missions = () => (
+  <div className="mission__container">
+    <table>
+      <tr>
+        <th>Mission</th>
+        <th>Description</th>
+        <th>Status</th>
+        <th>{' '}</th>
+      </tr>
+    </table>
+  </div>
+);
+
+export default Missions;
