3eedae147cc54c7d75ded96a377c982ae451e566
arthurGC <aguzmanc@unsa.edu.pe>
2021-08-30 18:55:09 -0500

    Add styles to mission container component

--- src/components/MissionsPage.js
+++ src/components/MissionsPage.js
@@ -1,13 +1,14 @@
 import React from 'react';
+import style from './mission.module.css';
 
 const MissionsPage = () => (
   <div className="mission__container">
-    <table>
-      <tr>
-        <th>Mission</th>
-        <th>Description</th>
-        <th>Status</th>
-        <th>{' '}</th>
+    <table className={style.table}>
+      <tr className={style.row}>
+        <th className={style.column}>Mission</th>
+        <th className={style.column}>Description</th>
+        <th className={style.column}>Status</th>
+        <th className={style.column}>{' '}</th>
       </tr>
     </table>
   </div>
