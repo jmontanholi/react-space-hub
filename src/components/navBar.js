3fb77f29edd9fb5408a794b346ca27763a38e69a
jmontanholi <jonunes9326@hotmail.com.br>
2021-08-30 17:05:23 -0300

    Finish fetching rockets data and storing it

--- src/components/navBar.js
+++ src/components/navBar.js
@@ -1,21 +1,29 @@
 /* eslint-disable react/no-unescaped-entities */
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import { NavLink } from 'react-router-dom';
 import logo from '../planet.png';
 import style from './navBar.module.css';
+import getRockets from '../redux/slices/rocketsSlice';
 
-const NavBar = () => (
-  <nav className={style.nav}>
-    <div className={style.logoDiv}>
-      <img className={style.img} src={logo} alt="planet with two moons and a ring" />
-      <h1 className={style.title}>Space Traveler's Hub</h1>
-    </div>
-    <div className={style.linkDiv}>
-      <NavLink className={style.link} exact activeClassName={style.selected} to="/">Rockets</NavLink>
-      <NavLink className={style.link} exact activeClassName={style.selected} to="/missions">Missions</NavLink>
-      <NavLink className={`${style.link} ${style.profile}`} exact activeClassName={style.selected} to="/profile">My Profile</NavLink>
-    </div>
-  </nav>
-);
+const NavBar = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getRockets());
+  }, []);
+  return (
+    <nav className={style.nav}>
+      <div className={style.logoDiv}>
+        <img className={style.img} src={logo} alt="planet with two moons and a ring" />
+        <h1 className={style.title}>Space Traveler's Hub</h1>
+      </div>
+      <div className={style.linkDiv}>
+        <NavLink className={style.link} exact activeClassName={style.selected} to="/">Rockets</NavLink>
+        <NavLink className={style.link} exact activeClassName={style.selected} to="/missions">Missions</NavLink>
+        <NavLink className={`${style.link} ${style.profile}`} exact activeClassName={style.selected} to="/profile">My Profile</NavLink>
+      </div>
+    </nav>
+  );
+};
 
 export default NavBar;
---
+++ src/redux/rockets/rockets.js
@@ -0,0 +1,36 @@
+import { GET_ROCKETS, GET_ROCKETS_SUCCESS, GET_ROCKETS_ERR } from '../slices/rocketsSlice';
+
+const initialState = {
+  rockets: [],
+  loading: false,
+  error: null,
+};
+
+const populateRockets = (data) => {
+  const arr = [];
+  for (let i = 0; i < data.length; i += 1) {
+    arr.push({
+      id: data[i].id,
+      name: data[i].rocket_name,
+      type: data[i].rocket_type,
+      flickr_images: data[i].flickr_images,
+    });
+  }
+  return arr;
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case GET_ROCKETS:
+      return { ...state, loading: true };
+    case GET_ROCKETS_SUCCESS:
+      populateRockets(action.data);
+      return { ...state, loading: false, rockets: populateRockets(action.data) };
+    case GET_ROCKETS_ERR:
+      return { ...state, loading: false, error: action.error };
+    default:
+      return state;
+  }
+};
+
+export default reducer;
