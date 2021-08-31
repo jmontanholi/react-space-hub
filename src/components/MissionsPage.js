9b10103152b827c71e6b9f0fa77fa90ec55afc12
arthurGC <aguzmanc@unsa.edu.pe>
2021-08-31 01:10:45 -0500

    Import Mission component

--- src/components/MissionsPage.js
+++ src/components/MissionsPage.js
@@ -1,28 +1,25 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import Mission from './Mission';
 import style from './missionpage.module.css';
 
 const MissionsPage = () => {
-  const missions = [];
+  const missions = useSelector((state) => state.missions.missions);
 
   return (
     <div className="mission__container">
       <table className={style.table}>
         <thead>
-          <tr className={style.row}>
-            <th className={style.column}>Mission</th>
-            <th className={style.column}>Description</th>
-            <th className={style.column}>Status</th>
-            <th className={style.column}>{' '}</th>
+          <tr>
+            <th className={`${style.column} ${style.head}`}>Mission</th>
+            <th className={`${style.column} ${style.head}`}>Description</th>
+            <th className={`${style.column} ${style.head}`}>Status</th>
+            <th className={`${style.column} ${style.head}`}>{' '}</th>
           </tr>
         </thead>
         <tbody>
           {missions.map((mission) => (
-            <tr className={style.row} key={mission.id}>
-              <td className={style.column}>{mission.name}</td>
-              <td className={style.column}>{mission.description}</td>
-              <td className={style.column}><button type="button">NOT A MEMBER</button></td>
-              <td className={style.column}><button type="button">Join Mission</button></td>
-            </tr>
+            <Mission key={mission.id} name={mission.name} description={mission.description} />
           ))}
         </tbody>
       </table>
