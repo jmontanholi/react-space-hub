1ce4d65219156aa5e4cc6ff4a14afdbb2620ea0a
arthurGC <aguzmanc@unsa.edu.pe>
2021-08-30 19:59:03 -0500

    Add missionReducer

--- src/redux/createStore.js
+++ src/redux/createStore.js
@@ -3,9 +3,11 @@ import { createStore, combineReducers, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
 import ReduxThunk from 'redux-thunk';
 import rocketsReducer from './rockets/rockets';
+import missionsReducer from './missions/missions';
 
 const reducer = combineReducers({
   rockets: rocketsReducer,
+  missions: missionsReducer,
 });
 
 const store = createStore(
