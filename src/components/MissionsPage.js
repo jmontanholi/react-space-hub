c07f9143e0bdaffc2d893de547968b91d9a8c3fa
arthurGC <aguzmanc@unsa.edu.pe>
2021-08-30 19:59:36 -0500

    Execute use Mission to check functionality

--- src/components/MissionsPage.js
+++ src/components/MissionsPage.js
@@ -1,19 +1,24 @@
 import React from 'react';
+import useMission from '../redux/hooks/useMission';
 import style from './mission.module.css';
 
-const MissionsPage = () => (
-  <div className="mission__container">
-    <table className={style.table}>
-      <thead>
-        <tr className={style.row}>
-          <th className={style.column}>Mission</th>
-          <th className={style.column}>Description</th>
-          <th className={style.column}>Status</th>
-          <th className={style.column}>{' '}</th>
-        </tr>
-      </thead>
-    </table>
-  </div>
-);
+const MissionsPage = () => {
+  useMission();
+
+  return (
+    <div className="mission__container">
+      <table className={style.table}>
+        <thead>
+          <tr className={style.row}>
+            <th className={style.column}>Mission</th>
+            <th className={style.column}>Description</th>
+            <th className={style.column}>Status</th>
+            <th className={style.column}>{' '}</th>
+          </tr>
+        </thead>
+      </table>
+    </div>
+  );
+};
 
 export default MissionsPage;
