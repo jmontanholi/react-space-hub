beb99a8d44fc640b63b0e75071f9491e13cf9139
arthurGC <aguzmanc@unsa.edu.pe>
2021-09-01 10:09:52 -0500

    Add msg when profile is empty

--- src/components/myProfile.js
+++ src/components/myProfile.js
@@ -11,6 +11,8 @@ const MyProfile = () => {
   const missions = useSelector((state) => state.missions.missions);
   const missionsFiltered = missions.filter((mission) => mission.reserved === true);
   const dispatch = useDispatch();
+  const missionEmpty = missionsFiltered.length <= 0;
+  const rocketsEmpty = filtered.length <= 0;
 
   return (
     <div className={style.div}>
@@ -18,20 +20,22 @@ const MyProfile = () => {
         <div className={style.missionsDiv}>
           <h3 className={style.missionsTitle}>Missions Joined</h3>
           <ul className={style.ul}>
-            {
-              missionsFiltered.map((mission) => (
-                <li className={style.li} key={mission.id}>
-                  <h3 className={style.rocketName}>{mission.name}</h3>
-                  { mission.reserved
+            { missionEmpty
+            && <li className={style.msg}>NO MISSIONS JOINED</li>}
+            {missionsFiltered.map((mission) => (
+              <li className={style.li} key={mission.id}>
+                <h3 className={style.rocketName}>{mission.name}</h3>
+                { mission.reserved
                   && <Button variant="outline-secondary" className={style.rocketBtn} onClick={() => { dispatch(unreserveMission(mission.id)); }}>Leave Mission</Button>}
-                </li>
-              ))
-            }
+              </li>
+            ))}
           </ul>
         </div>
         <div className={style.rocketsDiv}>
           <h3 className={style.rocketsTitle}>Rockets Reserved</h3>
           <ul className={style.ul}>
+            {rocketsEmpty
+            && <li className={style.msg}>NO ROCKETS RESERVED</li>}
             {
               filtered.map((rocket) => (
                 <li className={style.li} key={rocket.id}>
